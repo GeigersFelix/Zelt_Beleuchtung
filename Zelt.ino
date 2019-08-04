99f5076a8a637d59cc85a9594968610769e1400d
GeigersFelix <32980135+GeigersFelix@users.noreply.github.com>
2019-08-04 10:10:55 +0200

    Update Zelt.ino

--- Zelt.ino
+++ Zelt.ino
@@ -10,17 +10,28 @@ int raum[2][12] = {
   {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17}
 };
 
+int regenbogenindex[24]={
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
+};
+
+int rainbowcolors[3][40]={
+  {0, 128, 130, 141, 153, 153, 128, 101, 72, 4, 0, 1, 1, 11, 23, 0, 0, 128, 200, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 168, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 40, 83, 102, 102, 0, 0, 0, 0, 68, 114, 159, 175, 179, 212, 255, 255, 255, 255, 219, 182, 146, 109, 73, 0, 0, 105, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 40, 59, 117, 169, 128, 155, 225, 208, 220, 226, 232, 162, 77, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 180, 255, 185, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+//http://wiki.baw.de/de/index.php/Farbverlauf:_Regenbogen,_29_Farben
+
 int raumleds[2]={
   12, 12};
 
 char gelesen;
-char ani_kl='a';
 char ani_gr='A';
-int mod=1;
 int runvar[3]={
   0, 0, 0};
 
 int animod=0;
+int aniregenfarb=0;
 
 int tastermod=2;
 int tasterani=3;
@@ -41,21 +52,6 @@ void loop()
     animod=1;
   }
 
-  if(digitalRead(tastermod)==HIGH)
-  {
-    mod++;
-    if(mod>2)
-    {
-      mod=1;
-    }
-    else
-    {
-      ani_kl='a';
-    }
-    while(digitalRead(tastermod)==HIGH)
-    {}
-  }
-
   if(digitalRead(tasterani)==HIGH)
   {
     ani_gr++;
@@ -71,150 +67,62 @@ void loop()
   {
     gelesen=Serial.read();
     Serial.println(gelesen);
+    ani_gr=gelesen;
   }
-  
-  switch(gelesen)
+   
+  switch(ani_gr)
   {
-    case 'a':
-    case 'b':
-    case 'c':
-    case 'd':
-    case 'e':
-    case 'f':
-    case 'g':
-    case 'h':
-    case 'i':
-      ani_kl=gelesen;
-      break;
     case 'A':
+      setzen(0, 0, 0);
+      break;
     case 'B':
+      setzen(255, 0, 0);
+      break;
     case 'C':
+      setzen(0, 255, 0);
+      break;
     case 'D':
+      setzen(0, 0, 255);
+      break;
     case 'E':
+      setzen(255, 255, 255);
+      break;
     case 'F':
-      Serial.println("case F");
+      rainbow(20);
+      break;
     case 'G':
+      
     case 'H':
+      rgbrun(animod);
+      break;
     case 'I':
-      ani_gr=gelesen;
+      runpix(255, 255, 0, 2);
       break;
-    case '1':
-      mod=1;
-      break;
-    case '2':
-      mod=2;
+    case 'J':
+      theaterChase(pixels.Color(255, 0, 0), 50);
       break;
   }
-
-  switch(mod)
-  {
-    case 1:
-      switch(ani_gr)
-      {
-        case 'A':
-          setzen(0, 0, 0, 2);
-          break;
-        case 'B':
-          setzen(255, 0, 0, 2);
-          break;
-        case 'C':
-          setzen(0, 255, 0, 2);
-          break;
-        case 'D':
-          setzen(0, 0, 255, 2);
-          break;
-        case 'E':
-          setzen(255, 255, 255, 2);
-          break;
-        case 'F':
-          rainbow(20);
-          Serial.println("case F mod groß");
-          break;
-        case 'G':
-          rgbrun(animod, 2);
-          break;
-        case 'H':
-          runpix(255, 255, 0, 2);
-          break;
-        case 'I':
-          theaterChase(pixels.Color(255, 0, 0), 50);
-          break;
-      }
-      break;
-    case 2:
-      switch(ani_gr)
-      {
-        case 'A':
-          setzen(0, 0, 0, 0);
-          break;
-        case 'B':
-          setzen(255, 0, 0, 0);
-          break;
-        case 'C':
-          setzen(0, 255, 0, 0);
-          break;
-        case 'D':
-          setzen(0, 0, 255, 0);
-          break;
-        case 'E':
-          setzen(255, 255, 255, 0);
-          break;
-        case 'F':
-          rainbow2(20, 0);
-          break;
-        case 'G':
-          rgbrun(animod, 0);
-          break;
-        case 'H':
-          runpix(255, 255, 0, 0);
-          break;
-        case 'I':
-          theaterChase2(pixels.Color(255, 0, 0), 50, 0);
-          break;
-      }
-      switch(ani_kl)
-      {
-        case 'a':
-          setzen(0, 0, 0, 1);
-          break;
-        case 'b':
-          setzen(255, 0, 0, 1);
-          break;
-        case 'c':
-          setzen(0, 255, 0, 1);
-          break;
-        case 'd':
-          setzen(0, 0, 255, 1);
-          break;
-        case 'e':
-          setzen(255, 255, 255, 1);
-          break;
-        case 'f':
-          rainbow2(20, 1);
-          break;
-        case 'g':
-          rgbrun(animod, 1);
-          break;
-        case 'h':
-          runpix(255, 255, 0, 1);
-          break;
-        case 'i':
-          theaterChase2(pixels.Color(255, 0, 0), 50, 1);
-          break;
-      }
-      break;
-  }
+        
   Serial.print("ani_gr: ");
   Serial.println(ani_gr);
-  Serial.print("ani_kl: ");
-  Serial.println(ani_kl);
-  Serial.print("mod: ");
-  Serial.println(mod);
   Serial.print("animod: ");
   Serial.println(animod);
   Serial.print("gelesen: ");
   Serial.println(gelesen);
   pixels.show();
+}
+
+void regenkreisel()
+{
+  for(int i=0; i<24; i++)
+  {
+    pixels.setPixelColor(i, pixels.Color(rainbowcolors[0][regenbogenindex[i]], rainbowcolors[1][regenbogenindex[i]], rainbowcolors[2][regenbogenindex[i]]));
+    regenbogenindex[i]++;
+    if(regenbogenindex[i]>=30)
+    {
+      regenbogenindex[i]=0;
+    }
+  }
 }
 
 void theaterChase(uint32_t c, uint8_t wait) {
@@ -276,69 +184,34 @@ void runpix(int rot, int gruen, int blau, int mod)
   pixels.show();
 }
 
-void rgbrun(int farbe, int aufteilung)
+void rgbrun(int farbe)
 {
   Serial.println("rgbrun");
-  switch(aufteilung)
+  
+  for(int i=0; i<NUM_LEDS; i++)
   {
-    case 0:
-    case 1:
-      for(int i=0; i<raumleds[aufteilung]; i++)
-      {
-        switch(farbe)
-        {
-          case 1:
-            pixels.setPixelColor(raum[aufteilung][i], pixels.Color(255, 0, 0));
-            break;
-          case 2:
-            pixels.setPixelColor(raum[aufteilung][i], pixels.Color(0, 255, 0));
-            break;
-          case 3:
-            pixels.setPixelColor(raum[aufteilung][i], pixels.Color(0, 0, 255));
-            break;
-        }
-        pixels.show();
-        delay(50);
-      }
-      break;
-    case 2:
-      for(int i=0; i<NUM_LEDS; i++)
-      {
-        switch(farbe)
-        {
-          case 1:
-            pixels.setPixelColor(i, pixels.Color(255, 0, 0));
-            break;
-          case 2:
-            pixels.setPixelColor(i, pixels.Color(0, 255, 0));
-            break;
-          case 3:
-            pixels.setPixelColor(i, pixels.Color(0, 0, 255));
-            break;
-        }
-        pixels.show();
-        delay(50);
-      }
-      break;
+    switch(farbe)
+    {
+      case 1:
+        pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+        break;
+      case 2:
+        pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+        break;
+      case 3:
+        pixels.setPixelColor(i, pixels.Color(0, 0, 255));
+        break;
+    }
+    pixels.show();
+    delay(50);
   }
 }
 
-void setzen(int rot, int gruen, int blau, int leds)
+void setzen(int rot, int gruen, int blau)
 {
-  switch(leds)
+  for(int i=0; i<NUM_LEDS; i++)
   {
-    case 0:
-    case 1:
-      for(int i=0; i<raumleds[leds]; i++)
-      {
-        pixels.setPixelColor(raum[leds][i], pixels.Color(rot, gruen, blau));
-      }
-      break;
-    case 2:
-      for(int i=0; i<NUM_LEDS; i++)
-      {
-        pixels.setPixelColor(i, pixels.Color(rot, gruen, blau));
-      }
+    pixels.setPixelColor(i, pixels.Color(rot, gruen, blau));
   }
   pixels.show();
 }
